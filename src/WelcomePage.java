8dfb30278b343b085e9952543e60293ed4fd0928
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-15 10:01:51 +0300

    Created constructor the to welcome page; Now it opens a a window when logged in

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -1,2 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class WelcomePage {
+
+    JFrame frame = new JFrame();
+    JLabel welcomeLabel = new JLabel("You have logged in successfully");
+
+    WelcomePage() {
+
+        welcomeLabel.setBounds(30, 150, 350, 35);
+        welcomeLabel.setFont(new Font("Arial", Font.PLAIN, 25));
+        welcomeLabel.setForeground(Color.green);
+
+        frame.add(welcomeLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
