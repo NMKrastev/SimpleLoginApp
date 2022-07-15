71da69b04b0dd4914ec0fe76b5382c1369c62327
Nikola <nikola.malinov.krastev@gmail.com>
2022-07-15 09:19:55 +0300

    Added construtor in LoginPage class; Created frame and labels for the usedID and password

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,6 +1,38 @@
-public class LoginPage {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-    LoginPage() {
+public class LoginPage implements ActionListener {
+
+    JFrame frame = new JFrame("Login");
+    JButton loginButton = new JButton("Login");
+    JButton resetButton = new JButton("Reset");
+    JTextField userIDField = new JTextField();
+    JPasswordField userPasswordField = new JPasswordField();
+    JLabel userIDLabel = new JLabel("User ID:");
+    JLabel userPasswordLabel = new JLabel("Password:");
+    JLabel messageLabel = new JLabel();
+    HashMap<String, String> loginInfo = new HashMap<String, String>();
+
+    LoginPage(HashMap<String, String> loginInfoOrigin) {
+
+        loginInfo = loginInfoOrigin;
+
+        userIDLabel.setBounds(50, 100, 75, 25);
+        userPasswordLabel.setBounds(50, 150, 75, 25);
+
+        frame.add(userIDLabel);
+        frame.add(userPasswordLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
